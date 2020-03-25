70bfd3a7ec6cc8cd9aafda4377f76b033d1fda38
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-25 17:14:33 -0400

    fix tag statement

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "aws-cloud-automation-meetup-ec2" {
   ami = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
 
-  tags {
+  tags = {
     Owner = "terraform-aws-cloud-automation-meetup"
     Project = "aws-cloud-automation-meetup"
   }
