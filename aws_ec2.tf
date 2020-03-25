e1ce68b655e76b49ce376e86551ac20c06270ee1
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-25 17:34:12 -0400

    add inbound route for port 80

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -11,6 +11,14 @@ resource "aws_security_group" "aws-cloud-automation-meetup-sg" {
       "0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+    cidr_blocks = [
+      "0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     protocol = "-1"
